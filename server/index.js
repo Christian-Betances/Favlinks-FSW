3517fd1a9bf68218c83a6dbc304cd57523661e9f
Christian-Betances <chrisbet313@gmail.com>
2023-12-11 22:14:55 -0500

    connected server to postgres

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ const path = require('path')
 
 const app = express();
 
+const db = require('./queries.js')
+
 const PORT = 3000
 
 const clientPath = path.resolve(__dirname, '../client/dist')
@@ -14,6 +16,8 @@ app.use(express.static(clientPath))
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'))
     })
+
+app.get('/api/links', db.getLinks)
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
